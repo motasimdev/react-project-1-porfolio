8849e67fe5c86b2eef52cb238082cbdbb3dd9aeb
barakahtrust <barakahtrustbd@gmail.com>
2025-06-29 12:21:35 +0600

    aa

--- src/components/layouts/Banner.jsx
+++ src/components/layouts/Banner.jsx
@@ -1,28 +1,56 @@
-import React from 'react'
-import Container from '../Container'
-import Flex from '../Flex'
-import Heading from '../Heading'
-import Button from '../Button'
+import React from "react";
+import Container from "../Container";
+import Flex from "../Flex";
+import Heading from "../Heading";
+import Button from "../Button";
+import { MdMailOutline } from "react-icons/md";
+import { MdOutlineFileDownload } from "react-icons/md";
 
 const Banner = () => {
   return (
     <>
-    <section className={"py-4 bg-banner"}>
+      <section className={"py-4 bg-banner"}>
         <Container>
-            <Flex className={"justify-between"}>
-                <div className="">
-                  <Heading text={"SAKIB AL HASAN"} as={"h4"}/>
-                  <Heading text={"Hello, my name’s Sakib. I’m MERN Stack Developer."} as={"h1"}/>
-                  <Button  btnTxt={<>
-                    CONTACT ME
-                    
-                    </>}/>
-                </div>
-            </Flex>
+          <Flex className={"justify-between"}>
+            <div className="">
+              <Heading text={"SAKIB AL HASAN"} as={"h4"} className={"text-base uppercasse text-primaryClr font-pop font-medium"}/>
+              <Heading
+                text={"Hello, my name’s Sakib. I’m MERN Stack Developer."}
+                as={"h1"}
+              />
+              <Flex>
+                {/* ====================== */}
+                <Button
+                  className={
+                    "py-3 px-6 text-white font-pop text-sm font-medium uppercase flex items-center gap-x-2"
+                  }
+                  btnTxt={
+                    <>
+                      CONTACT ME
+                      <MdMailOutline />
+                    </>
+                  }
+                />
+                {/* ======================== */}
+                <Button
+                  className={
+                    "py-3 px-6 text-white font-pop text-sm font-medium uppercase flex items-center gap-x-2"
+                  }
+                  btnTxt={
+                    <>
+                      <MdOutlineFileDownload />
+                      DOWNLOAD CV
+                    </>
+                  }
+                />
+                {/* ================ */}
+              </Flex>
+            </div>
+          </Flex>
         </Container>
-    </section>
-    </> 
-  )
-}
+      </section>
+    </>
+  );
+};
 
-export default Banner
+export default Banner;
--- src/components/layouts/Header.jsx
+++ src/components/layouts/Header.jsx
@@ -1,40 +1,52 @@
-import React from 'react'
-import Container from '../Container'
-import Flex from '../Flex'
-import Image from '../Image'
-import logo from '../../assets/logo.png'
-import Menu from '../Menu'
-import Button from '../Button'
+import React from "react";
+import Container from "../Container";
+import Flex from "../Flex";
+import Image from "../Image";
+import logo from "../../assets/logo.png";
+import Menu from "../Menu";
+import Button from "../Button";
 import { MdMailOutline } from "react-icons/md";
 
 const Header = () => {
   return (
     <>
-    <section className={'py-2'}>
-       <Container>
-            <Flex className={`justify-between`}>
-                <div className="">
-                    <Image imgSrc={logo}/>
-                </div>
-                <div className="">
-                    <Menu className={`flex gap-5 font-pop uppercase text-sm tracking-widest tracking-widest`}>
-                        <li>Home</li>
-                        <li>About</li>
-                        <li>Service</li>
-                        <li>Contact</li>
-                    </Menu>
-                </div>
-                <div className="">
-                    <Button className={"py-3 px-6 text-white font-pop text-sm font-medium uppercase flex items-center gap-x-2"} btnTxt={<>
-                        hire me
-                        <MdMailOutline />
-                        </>}/>
-                </div>
-            </Flex>
-       </Container>
-    </section>
-    </>
-  )
-}
+      <section className={"py-2"}>
+        <Container>
+          <Flex className={`justify-between`}>
+            <div className="">
+              <Image imgSrc={logo} />
+            </div>
+            <div className="">
+              <Menu
+                className={`flex gap-5 font-pop uppercase text-sm tracking-widest tracking-widest`}
+              >
+                <li>Home</li>
+                <li>About</li>
+                <li>Service</li>
+                <li>Contact</li>
+              </Menu>
+            </div>
+            <div className="">
 
-export default Header
+                {/* =========================== */}
+              <Button
+                className={
+                  "py-3 px-6 text-white font-pop text-sm font-medium uppercase flex items-center gap-x-2"
+                }
+                btnTxt={
+                  <>
+                    hire me
+                    <MdMailOutline />
+                  </>
+                }
+              />
+              {/* =========================== */}
+            </div>
+          </Flex>
+        </Container>
+      </section>
+    </>
+  );
+};
+
+export default Header;
